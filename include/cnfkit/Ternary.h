0a95cb83979916ef4cdcad70fdb3db4209af2938
Felix Kutzner <felixkutzner@gmail.com>
2021-05-22 15:36:51 +0200

    removing *Impl.h

--- include/cnfkit/Ternary.h
+++ include/cnfkit/Ternary.h
@@ -27,6 +27,46 @@ constexpr tbool t_true = tbool{1u};
 constexpr tbool t_indet = tbool{2u};
 
 constexpr auto to_tbool(bool it) -> tbool;
+
+
+// *** Implementation ***
+
+constexpr auto tbool::operator&(tbool const& rhs) const -> tbool
+{
+  return (*this == t_true && rhs == t_true)
+             ? t_true
+             : ((*this == t_false || rhs == t_false) ? t_false : t_indet);
 }
 
-#include "cnfkit/TernaryImpl.h"
+constexpr auto tbool::operator|(tbool const& rhs) const -> tbool
+{
+  return *this == t_false && rhs == t_false ? t_false
+                                            : (*this == t_true || rhs == t_true ? t_true : t_indet);
+}
+
+constexpr auto tbool::operator!() const -> tbool
+{
+  return *this == t_indet ? t_indet : (*this == t_true ? t_false : t_true);
+}
+
+constexpr tbool::operator bool() const
+{
+  return *this == t_true;
+}
+
+constexpr auto tbool::operator==(tbool const& rhs) const -> bool
+{
+  return this == &rhs || m_value == rhs.m_value;
+}
+
+constexpr auto tbool::operator!=(tbool const& rhs) const -> bool
+{
+  return !(*this == rhs);
+}
+
+constexpr auto to_tbool(bool it) -> tbool
+{
+  return it ? t_true : t_false;
+}
+
+}
