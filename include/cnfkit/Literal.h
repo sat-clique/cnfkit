a5f40a18fcffaa48e3ca6e30cf383c61a6e9e9be
Felix Kutzner <felixkutzner@gmail.com>
2021-05-29 15:10:47 +0200

    adding missing noexcept specifiers

--- include/cnfkit/Literal.h
+++ include/cnfkit/Literal.h
@@ -6,14 +6,12 @@
 #include <cstdint>
 #include <limits>
 
-// TODO: add noexcept
-
 namespace cnfkit {
 
 class var {
 public:
-  constexpr var(uint32_t raw_value) : m_raw_value{raw_value} {}
-  constexpr auto get_raw_value() const -> uint32_t;
+  constexpr var(uint32_t raw_value) noexcept : m_raw_value{raw_value} {}
+  constexpr auto get_raw_value() const noexcept -> uint32_t;
 
   auto operator++(int) noexcept -> var&;
   auto operator++() noexcept -> var;
@@ -29,22 +27,22 @@ constexpr var invalid_var(max_raw_var + 1);
 
 class lit {
 public:
-  constexpr lit(var variable, bool is_positive);
+  constexpr lit(var variable, bool is_positive) noexcept;
 
-  constexpr auto get_var() const -> var;
-  constexpr auto is_positive() const -> bool;
+  constexpr auto get_var() const noexcept -> var;
+  constexpr auto is_positive() const noexcept -> bool;
 
-  constexpr auto operator-() const -> lit;
-  constexpr auto get_raw_value() const -> uint32_t;
-  constexpr auto is_valid() const -> bool;
+  constexpr auto operator-() const noexcept -> lit;
+  constexpr auto get_raw_value() const noexcept -> uint32_t;
+  constexpr auto is_valid() const noexcept -> bool;
 
   auto operator++(int) noexcept -> lit&;
   auto operator++() noexcept -> lit;
   auto operator--(int) noexcept -> lit&;
   auto operator--() noexcept -> lit;
 
-  constexpr auto next_with_same_sign() const -> lit;
-  constexpr auto prev_with_same_sign() const -> lit;
+  constexpr auto next_with_same_sign() const noexcept -> lit;
+  constexpr auto prev_with_same_sign() const noexcept -> lit;
 
 private:
   constexpr lit(uint32_t raw_value) : m_raw_value{raw_value} {}
@@ -52,43 +50,43 @@ private:
 };
 
 namespace cnfkit_literals {
-constexpr lit operator"" _lit(unsigned long long n);
-constexpr var operator"" _var(unsigned long long n);
+constexpr auto operator"" _lit(unsigned long long n) noexcept -> lit;
+constexpr auto operator"" _var(unsigned long long n) noexcept -> var;
 }
 
 constexpr auto lit_to_dimacs(lit const& lit) noexcept -> int32_t;
 inline auto dimacs_to_lit(int32_t dimacs_lit) noexcept -> lit;
 
-constexpr auto operator==(var const& lhs, var const& rhs) -> bool;
-constexpr auto operator!=(var const& lhs, var const& rhs) -> bool;
-constexpr auto operator<(var const& lhs, var const& rhs) -> bool;
-constexpr auto operator<=(var const& lhs, var const& rhs) -> bool;
-constexpr auto operator>(var const& lhs, var const& rhs) -> bool;
-constexpr auto operator>=(var const& lhs, var const& rhs) -> bool;
+constexpr auto operator==(var const& lhs, var const& rhs) noexcept -> bool;
+constexpr auto operator!=(var const& lhs, var const& rhs) noexcept -> bool;
+constexpr auto operator<(var const& lhs, var const& rhs) noexcept -> bool;
+constexpr auto operator<=(var const& lhs, var const& rhs) noexcept -> bool;
+constexpr auto operator>(var const& lhs, var const& rhs) noexcept -> bool;
+constexpr auto operator>=(var const& lhs, var const& rhs) noexcept -> bool;
 
-constexpr auto operator==(lit const& lhs, lit const& rhs) -> bool;
-constexpr auto operator!=(lit const& lhs, lit const& rhs) -> bool;
-constexpr auto operator<(lit const& lhs, lit const& rhs) -> bool;
-constexpr auto operator<=(lit const& lhs, lit const& rhs) -> bool;
-constexpr auto operator>(lit const& lhs, lit const& rhs) -> bool;
-constexpr auto operator>=(lit const& lhs, lit const& rhs) -> bool;
+constexpr auto operator==(lit const& lhs, lit const& rhs) noexcept -> bool;
+constexpr auto operator!=(lit const& lhs, lit const& rhs) noexcept -> bool;
+constexpr auto operator<(lit const& lhs, lit const& rhs) noexcept -> bool;
+constexpr auto operator<=(lit const& lhs, lit const& rhs) noexcept -> bool;
+constexpr auto operator>(lit const& lhs, lit const& rhs) noexcept -> bool;
+constexpr auto operator>=(lit const& lhs, lit const& rhs) noexcept -> bool;
 
 
 // *** Implementation ***
 
 namespace cnfkit_literals {
-constexpr lit operator"" _lit(unsigned long long n)
+constexpr auto operator"" _lit(unsigned long long n) noexcept -> lit
 {
   return lit{var{static_cast<uint32_t>(n)}, true};
 }
 
-constexpr var operator"" _var(unsigned long long n)
+constexpr auto operator"" _var(unsigned long long n) noexcept -> var
 {
   return var{static_cast<uint32_t>(n)};
 }
 }
 
-constexpr auto var::get_raw_value() const -> uint32_t
+constexpr auto var::get_raw_value() const noexcept -> uint32_t
 {
   return m_raw_value;
 }
@@ -119,33 +117,33 @@ inline auto var::operator--() noexcept -> var
   return *this;
 }
 
-constexpr lit::lit(var variable, bool is_positive)
+constexpr lit::lit(var variable, bool is_positive) noexcept
   : m_raw_value{(variable.get_raw_value() << 1) + (is_positive ? 1 : 0)}
 {
 }
 
-constexpr auto lit::get_var() const -> var
+constexpr auto lit::get_var() const noexcept -> var
 {
   return var{m_raw_value >> 1};
 }
 
-constexpr auto lit::is_positive() const -> bool
+constexpr auto lit::is_positive() const noexcept -> bool
 {
   return (m_raw_value & 1) == 1;
 }
 
-constexpr auto lit::operator-() const -> lit
+constexpr auto lit::operator-() const noexcept -> lit
 {
   return lit{m_raw_value ^ 1};
 }
 
 
-constexpr auto lit::get_raw_value() const -> uint32_t
+constexpr auto lit::get_raw_value() const noexcept -> uint32_t
 {
   return m_raw_value;
 }
 
-constexpr auto lit::is_valid() const -> bool
+constexpr auto lit::is_valid() const noexcept -> bool
 {
   return get_var() != invalid_var;
 }
@@ -176,12 +174,12 @@ inline auto lit::operator--() noexcept -> lit
   return *this;
 }
 
-constexpr auto lit::next_with_same_sign() const -> lit
+constexpr auto lit::next_with_same_sign() const noexcept -> lit
 {
   return lit{m_raw_value + 2};
 }
 
-constexpr auto lit::prev_with_same_sign() const -> lit
+constexpr auto lit::prev_with_same_sign() const noexcept -> lit
 {
   return lit{m_raw_value - 2};
 }
@@ -206,62 +204,62 @@ inline auto dimacs_to_lit(int32_t dimacs_lit) noexcept -> lit
 }
 
 
-constexpr auto operator==(var const& lhs, var const& rhs) -> bool
+constexpr auto operator==(var const& lhs, var const& rhs) noexcept -> bool
 {
   return &lhs == &rhs || lhs.get_raw_value() == rhs.get_raw_value();
 }
 
-constexpr auto operator!=(var const& lhs, var const& rhs) -> bool
+constexpr auto operator!=(var const& lhs, var const& rhs) noexcept -> bool
 {
   return !(lhs == rhs);
 }
 
-constexpr auto operator<(var const& lhs, var const& rhs) -> bool
+constexpr auto operator<(var const& lhs, var const& rhs) noexcept -> bool
 {
   return lhs.get_raw_value() < rhs.get_raw_value();
 }
 
-constexpr auto operator<=(var const& lhs, var const& rhs) -> bool
+constexpr auto operator<=(var const& lhs, var const& rhs) noexcept -> bool
 {
   return lhs.get_raw_value() <= rhs.get_raw_value();
 }
 
-constexpr auto operator>(var const& lhs, var const& rhs) -> bool
+constexpr auto operator>(var const& lhs, var const& rhs) noexcept -> bool
 {
   return lhs.get_raw_value() > rhs.get_raw_value();
 }
 
-constexpr auto operator>=(var const& lhs, var const& rhs) -> bool
+constexpr auto operator>=(var const& lhs, var const& rhs) noexcept -> bool
 {
   return lhs.get_raw_value() >= rhs.get_raw_value();
 }
 
-constexpr auto operator==(lit const& lhs, lit const& rhs) -> bool
+constexpr auto operator==(lit const& lhs, lit const& rhs) noexcept -> bool
 {
   return lhs.get_raw_value() == rhs.get_raw_value();
 }
 
-constexpr auto operator!=(lit const& lhs, lit const& rhs) -> bool
+constexpr auto operator!=(lit const& lhs, lit const& rhs) noexcept -> bool
 {
   return lhs.get_raw_value() != rhs.get_raw_value();
 }
 
-constexpr auto operator<(lit const& lhs, lit const& rhs) -> bool
+constexpr auto operator<(lit const& lhs, lit const& rhs) noexcept -> bool
 {
   return lhs.get_raw_value() < rhs.get_raw_value();
 }
 
-constexpr auto operator<=(lit const& lhs, lit const& rhs) -> bool
+constexpr auto operator<=(lit const& lhs, lit const& rhs) noexcept -> bool
 {
   return lhs.get_raw_value() <= rhs.get_raw_value();
 }
 
-constexpr auto operator>(lit const& lhs, lit const& rhs) -> bool
+constexpr auto operator>(lit const& lhs, lit const& rhs) noexcept -> bool
 {
   return lhs.get_raw_value() > rhs.get_raw_value();
 }
 
-constexpr auto operator>=(lit const& lhs, lit const& rhs) -> bool
+constexpr auto operator>=(lit const& lhs, lit const& rhs) noexcept -> bool
 {
   return lhs.get_raw_value() >= rhs.get_raw_value();
 }
--- include/cnfkit/Ternary.h
+++ include/cnfkit/Ternary.h
@@ -8,17 +8,17 @@ namespace cnfkit {
 
 class tbool {
 public:
-  constexpr tbool() : m_value(0) {}
-  constexpr tbool(uint8_t value) : m_value(value) {}
+  constexpr tbool() noexcept : m_value(0) {}
+  constexpr tbool(uint8_t value) noexcept : m_value(value) {}
 
-  constexpr auto operator&(tbool const& rhs) const -> tbool;
-  constexpr auto operator|(tbool const& rhs) const -> tbool;
-  constexpr auto operator!() const -> tbool;
+  constexpr auto operator&(tbool const& rhs) const noexcept -> tbool;
+  constexpr auto operator|(tbool const& rhs) const noexcept -> tbool;
+  constexpr auto operator!() const noexcept -> tbool;
 
-  constexpr operator bool() const;
+  constexpr operator bool() const noexcept;
 
-  constexpr auto operator==(tbool const& rhs) const -> bool;
-  constexpr auto operator!=(tbool const& rhs) const -> bool;
+  constexpr auto operator==(tbool const& rhs) const noexcept -> bool;
+  constexpr auto operator!=(tbool const& rhs) const noexcept -> bool;
 
 private:
   uint8_t m_value;
@@ -28,45 +28,45 @@ constexpr tbool t_false = tbool{0u};
 constexpr tbool t_true = tbool{1u};
 constexpr tbool t_indet = tbool{2u};
 
-constexpr auto to_tbool(bool it) -> tbool;
+constexpr auto to_tbool(bool it) noexcept -> tbool;
 
 
 // *** Implementation ***
 
-constexpr auto tbool::operator&(tbool const& rhs) const -> tbool
+constexpr auto tbool::operator&(tbool const& rhs) const noexcept -> tbool
 {
   return (*this == t_true && rhs == t_true)
              ? t_true
              : ((*this == t_false || rhs == t_false) ? t_false : t_indet);
 }
 
-constexpr auto tbool::operator|(tbool const& rhs) const -> tbool
+constexpr auto tbool::operator|(tbool const& rhs) const noexcept -> tbool
 {
   return *this == t_false && rhs == t_false ? t_false
                                             : (*this == t_true || rhs == t_true ? t_true : t_indet);
 }
 
-constexpr auto tbool::operator!() const -> tbool
+constexpr auto tbool::operator!() const noexcept -> tbool
 {
   return *this == t_indet ? t_indet : (*this == t_true ? t_false : t_true);
 }
 
-constexpr tbool::operator bool() const
+constexpr tbool::operator bool() const noexcept
 {
   return *this == t_true;
 }
 
-constexpr auto tbool::operator==(tbool const& rhs) const -> bool
+constexpr auto tbool::operator==(tbool const& rhs) const noexcept -> bool
 {
   return this == &rhs || m_value == rhs.m_value;
 }
 
-constexpr auto tbool::operator!=(tbool const& rhs) const -> bool
+constexpr auto tbool::operator!=(tbool const& rhs) const noexcept -> bool
 {
   return !(*this == rhs);
 }
 
-constexpr auto to_tbool(bool it) -> tbool
+constexpr auto to_tbool(bool it) noexcept -> tbool
 {
   return it ? t_true : t_false;
 }
