6ab2a8828712903a1842600a8f7fee9a7fbb785f
Felix Kutzner <felixkutzner@gmail.com>
2021-05-30 11:58:45 +0200

    adding documentation for DIMACS and DRAT parsers

--- include/cnfkit/DimacsParser.h
+++ include/cnfkit/DimacsParser.h
@@ -1,5 +1,9 @@
 #pragma once
 
+/**
+ * \file
+ */
+
 #include <cnfkit/detail/CheckCxxVersion.h>
 
 #include <cnfkit/Literal.h>
@@ -9,14 +13,67 @@
 #include <filesystem>
 #include <string>
 
+/**
+ * \defgroup dimacs_parsers DIMACS CNF Parsers
+ *
+ * \brief Streaming parsers for DIMACS CNF problem instances
+ *
+ * The DIMACS CNF format is described in
+ * https://www.cs.utexas.edu/users/moore/acl2/manuals/current/manual/index-seo.php/SATLINK____DIMACS
+ *
+ * Notes:
+ *  - the CNF parser supports literals in the range `[-2^32 + 1, 2^32 - 1]`.
+ *  - since CNF writers don't handle the variable-count field in the DIMACS
+ *    header consistently (number of distinct variables vs. maximum occurring
+ *    variable), the value of the variable-count field is ignored by the CNF
+ *    parser, though it requires the value to be a 32-bit signed integer.
+ *  - the parser does not require comments to start at the beginning of lines,
+ *    though they may not start within the DIMACS header.
+ */
 
 namespace cnfkit {
+
+/**
+ * Parses a file containing a DIMACS CNF problem instance.
+ *
+ * \ingroup dimacs_parsers
+ *
+ * \param file               Path of the file to be parsed.
+ * \param clause_receiver    A function with signature `void(std::vector<lit> const&)`.
+ *                           `clause_receiver` is invoked for each parsed clause. `clause_receiver` may
+ *                           throw. Exceptions thrown by `clause_receiver` are not caught by the parser.
+ *
+ * \throws std::invalid_argument   when parsing the input failed.
+ */
 template <typename UnaryFn>
 void parse_cnf_file(std::filesystem::path const& input_file, UnaryFn&& clause_receiver);
 
+/**
+ * Parses a DIMACS CNF problem instance from stdin.
+ *
+ * \ingroup dimacs_parsers
+ *
+ * \param clause_receiver    A function with signature `void(std::vector<lit> const&)`.
+ *                           `clause_receiver` is invoked for each parsed clause. `clause_receiver` may
+ *                           throw. Exceptions thrown by `clause_receiver` are not caught by the parser.
+ *
+ * \throws std::invalid_argument   when parsing the input failed.
+ */
 template <typename UnaryFn>
 void parse_cnf_from_stdin(UnaryFn&& clause_receiver);
 
+/**
+ * Parses a string containing a DIMACS CNF problem instance.
+ *
+ * \ingroup dimacs_parsers
+ *
+ * \param cnf                The string to be parsed.
+ * \param clause_receiver    A function with signature `void(std::vector<lit> const&)`.
+ *                           `clause_receiver` is invoked for each parsed clause. `clause_receiver` may
+ *                           throw. Exceptions thrown by `clause_receiver` are not caught by the parser.
+ *
+ * \throws std::invalid_argument   when parsing the input failed.
+ */
 template <typename UnaryFn>
 void parse_cnf_string(std::string const& cnf, UnaryFn&& clause_receiver);
 
--- include/cnfkit/DratParser.h
+++ include/cnfkit/DratParser.h
@@ -1,5 +1,9 @@
 #pragma once
 
+/**
+ * \file
+ */
+
 #include <cnfkit/detail/CheckCxxVersion.h>
 
 #include <cnfkit/Literal.h>
@@ -9,19 +13,80 @@
 #include <filesystem>
 #include <string>
 
+/**
+ * \defgroup drat_parsers DRAT Proof Parsers
+ *
+ * \brief Streaming parsers for DRAT proofs
+ *
+ * Streaming parsers for DRAT proofs expressed in the drat-trim
+ * input format (see https://github.com/marijnheule/drat-trim)
+ *
+ * Notes:
+ *  - the DRAT parser supports literals in the range of in the range `[-2^32 + 1, 2^32 - 1]`.
+ *  - when parsing DRAT proofs expressed as DIMACS-like text, the parser accepts comments starting
+ *    anywhere in the file.
+ */
+
 namespace cnfkit {
 
+/**
+ * Parses a file containing a DRAT proof.
+ *
+ * \ingroup drat_parsers
+ *
+ * \param file               Path of the file to be parsed.
+ * \param format             If drat_format::text, the file is parsed as text DRAT, otherwise binary DRAT.
+ * \param clause_receiver    A function with signature `void(bool, std::vector<lit> const&)`.
+ *                           `clause_receiver` is invoked for each parsed clause. The first argument is true
+ *                           if and only if the clause is added to the proof.
+ *
+ * \throws std::invalid_argument   when parsing the input failed.
+ */
 template <typename BinaryFn>
 void parse_drat_file(std::filesystem::path const& file,
                      drat_format format,
                      BinaryFn&& clause_receiver);
 
+/**
+ * Parses a DRAT proof from stdin.
+ *
+ * \ingroup drat_parsers
+ *
+ * \param format             If drat_format::text, stdin is parsed as text DRAT, otherwise binary DRAT.
+ * \param clause_receiver    A function with signature `void(bool, std::vector<lit> const&)`.
+ *                           `clause_receiver` is invoked for each parsed clause. The first argument is true
+ *                           if and only if the clause is added to the proof.
+ *
+ * \throws std::invalid_argument   when parsing the input failed.
+ */
 template <typename BinaryFn>
 void parse_drat_from_stdin(drat_format format, BinaryFn&& clause_receiver);
 
+/**
+ * Parses a text DRAT proof given as a string.
+ *
+ * \ingroup drat_parsers
+ *
+ * \param clause_receiver    A function with signature `void(bool, std::vector<lit> const&)`.
+ *                           `clause_receiver` is invoked for each parsed clause. The first argument is true
+ *                           if and only if the clause is added to the proof.
+ *
+ * \throws std::invalid_argument   when parsing the input failed.
+ */
 template <typename BinaryFn>
 void parse_drat_string(std::string const& drat, BinaryFn&& clause_receiver);
 
+/**
+ * Parses a binary DRAT proof given as a contiguous sequence of bytes.
+ *
+ * \ingroup drat_parsers
+ *
+ * \param clause_receiver    A function with signature `void(bool, std::vector<lit> const&)`.
+ *                           `clause_receiver` is invoked for each parsed clause. The first argument is true
+ *                           if and only if the clause is added to the proof.
+ *
+ * \throws std::invalid_argument   when parsing the input failed.
+ */
 template <typename BinaryFn>
 void parse_drat_binary_buffer(std::byte const* start,
                               std::byte const* stop,
