527a533e0cb1ee538b1cd5088721bddb94a0e371
Felix Kutzner <felixkutzner@gmail.com>
2021-05-30 19:25:58 +0200

    adding source interface to io.h

--- include/cnfkit/io.h
+++ include/cnfkit/io.h
@@ -3,8 +3,17 @@
 #include <cnfkit/detail/check_cxx_version.h>
 
 #include <cstddef>
+#include <optional>
 
 namespace cnfkit {
+class source {
+public:
+  virtual auto read_bytes(std::byte* buf_start, std::byte* buf_stop) -> std::byte* = 0;
+  virtual auto read_byte() -> std::optional<std::byte> = 0;
+  virtual auto is_eof() -> bool = 0;
+  virtual ~source() = default;
+};
+
 class sink {
 public:
   virtual void write_bytes(std::byte const* start, std::byte const* stop) = 0;
@@ -12,4 +21,3 @@ public:
   virtual ~sink() = default;
 };
 }
-
